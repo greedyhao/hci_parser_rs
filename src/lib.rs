90279e3968160581e039c619d8946de3f6540030
ZHU Hao <hao_kr@163.com>
2023-09-17 21:05:04 +0800

    refactor(test):每层的测试代码丢自己下面

--- src/lib.rs
+++ src/lib.rs
@@ -67,27 +67,3 @@ impl ParseNodeInfo {
         ParseNodeInfo { name, sub_info }
     }
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn hci_cmd_ogf3_reset_test() {
-        use crate::hci;
-        use crate::HciPacket::*;
-        use crate::InnerStack;
-
-        let mut args = InnerStack::new();
-        let cmd = [0x03, 0x0c, 0x00];
-        let res = hci::parse(Cmd, &cmd, &mut args);
-
-        let res = res[0].to_json();
-        assert_eq!(
-            res.0,
-            r#"{"Opcode":"0xc03", "OCF":"0x3", "OGF":"0x3", "Command":"Reset", "Parameter_Total_Length":"0x0"}"#
-        );
-        assert_eq!(
-            res.1,
-            r#"{"Opcode":"(0,2)", "OCF":"(0,1)", "OGF","(1,1)", "Command":"(0,2)", "Parameter_Total_Length":"(2,1),0"}"#
-        );
-    }
-}
